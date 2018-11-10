d86c4a87423a848ddb1925db93dd36c551bf73a6
ExZiByte <exzibyte@gmail.com>
2018-11-10 14:58:14 -0600

    rewrite the clear command

--- src/main/java/com/nestedvariables/dev/Discord/Quiver/events/moderation/Clear.java
+++ src/main/java/com/nestedvariables/dev/Discord/Quiver/events/moderation/Clear.java
@@ -20,53 +20,57 @@ public class Clear extends ListenerAdapter {
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
         String[] args = event.getMessage().getContentRaw().split(" ");
 
-        if(args[0].equalsIgnoreCase(Info.PREFIX + "clear") || args[0].equalsIgnoreCase(Info.PREFIX + "c")){
-            if(event.getMember().hasPermission(Permission.MESSAGE_MANAGE)){
-                event.getMessage().delete().queue();
-
-                if(args.length < 2) {
+        if (args[0].equalsIgnoreCase(Info.PREFIX + "clear") || args[0].equalsIgnoreCase(Info.PREFIX + "c")) {
+            event.getMessage().delete().queue();
+            if (event.getMember().hasPermission(Permission.MESSAGE_MANAGE)) {
+                
+                if (args.length < 2) {
                     EmbedBuilder nullArgs1 = new EmbedBuilder();
 
-                    nullArgs1.setDescription(event.getMember().getAsMention() + ", you didn't specify the amount of messages to delete");
+                    nullArgs1.setDescription(":white_medium_small_square: " + event.getMember().getAsMention()
+                            + ", you didn't specify the amount of messages to delete");
                     nullArgs1.setColor(Info.ERROR_RED);
                     nullArgs1.setFooter("Quiver Undefined Message Count", Info.LOGO);
 
                     event.getChannel().sendMessage(nullArgs1.build()).queue((message) -> {
                         message.delete().queueAfter(10, TimeUnit.SECONDS);
                     });
-                }
-                
-                messageDeleteS = args[1];
-                messageDelete = Integer.parseInt(messageDeleteS);
-
-                if(messageDelete < 100) {
-                    Random random = new Random();
-                    int randomColor = random.nextInt(0xffffff + 1);
-
-                    List<Message> msgs = event.getChannel().getHistory().retrievePast(messageDelete).complete();
-                    event.getChannel().deleteMessages(msgs).queue();
-
-                    EmbedBuilder success = new EmbedBuilder();
-
-                    success.setDescription("Successfully deleted " + messageDelete + " messages from " + event.getChannel().getAsMention());
-                    success.setColor(randomColor);
-                    success.setFooter("Quiver Message Clear", Info.LOGO);
                 } else {
-                    EmbedBuilder tooManyMessages = new EmbedBuilder();
 
-                    tooManyMessages.setDescription(event.getMember().getAsMention() + ", the amount of messages you specified to delete was too many. \n Or the messages attempting to be deleted are older the 2 weeks. \n Maximum amount of messages you can delete at a time is 100.");
-                    tooManyMessages.setColor(Info.ERROR_RED);
-                    tooManyMessages.setFooter("Quiver Message Delete", Info.LOGO);
+                    messageDeleteS = args[1];
+                    messageDelete = Integer.parseInt(messageDeleteS);
 
-                    event.getChannel().sendMessage(tooManyMessages.build()).queue((message) -> {
-                        message.delete().queueAfter(15, TimeUnit.SECONDS);
-                    });
+                    if (messageDelete < 100) {
+                        Random random = new Random();
+                        int randomColor = random.nextInt(0xffffff + 1);
+
+                        List<Message> msgs = event.getChannel().getHistory().retrievePast(messageDelete).complete();
+                        event.getChannel().deleteMessages(msgs).queue();
+
+                        EmbedBuilder success = new EmbedBuilder();
+
+                        success.setDescription(":white_medium_small_square: Successfully deleted " + messageDelete
+                                + " messages from " + event.getChannel().getAsMention());
+                        success.setColor(randomColor);
+                        success.setFooter("Quiver Message Clear", Info.LOGO);                                               
+                    } else {
+                        EmbedBuilder tooManyMessages = new EmbedBuilder();
+
+                        tooManyMessages.setDescription(":white_medium_small_square: " + event.getMember().getAsMention()
+                                + ", the amount of messages you specified to delete were too many to delete at once. \n:white_medium_small_square: Or the messages attempting to be deleted are older the 2 weeks. \n :white_medium_small_square: Maximum amount of messages you can delete at a time is 100.");
+                        tooManyMessages.setColor(Info.ERROR_RED);
+                        tooManyMessages.setFooter("Quiver Message Delete", Info.LOGO);
+
+                        event.getChannel().sendMessage(tooManyMessages.build()).queue((message) -> {
+                            message.delete().queueAfter(15, TimeUnit.SECONDS);
+                        });
+                    }
                 }
-            }
-            else {
+            } else {
                 EmbedBuilder nullPerms = new EmbedBuilder();
 
-                nullPerms.setDescription(event.getMember().getAsMention() + ", you don't have sufficient permissions. \n You require the permission to manage messages to use this command.");
+                nullPerms.setDescription(":white_medium_small_square: " + event.getMember().getAsMention()
+                        + ", you don't have sufficient permissions. \n :white_medium_small_square: You require the permission to manage messages to use this command.");
                 nullPerms.setColor(Info.ERROR_RED);
                 nullPerms.setFooter("Quiver Insufficient Permissions", Info.LOGO);
 
