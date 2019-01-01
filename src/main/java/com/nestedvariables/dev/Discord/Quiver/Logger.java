ca9036ca9ed8e26548f55711873d9696f33c2aa9
Mykyta Komarn <nkomarn@hotmail.com>
2019-01-01 13:32:23 -0800

    Guild Logger null check

--- src/main/java/com/nestedvariables/dev/Discord/Quiver/Logger.java
+++ src/main/java/com/nestedvariables/dev/Discord/Quiver/Logger.java
@@ -7,23 +7,28 @@ public class Logger {
 
     // Logger method
     public static void log(String type, String info, Guild guild) {
-        switch (type) {
-            case "warning":
-                System.out.println(info);
-            case "fatal":
-                EmbedBuilder fatal = new EmbedBuilder();
-                fatal.setTitle(Utils.getMessage(guild, "fatalErrorEmbedTitle"));
-                fatal.setDescription(Utils.getMessage(guild, "fatalErrorEmbedDescription").replace("{error}", "```\n" + info + "\n```"));
-                fatal.setFooter(Utils.getMessage(guild, "name") + " " + Utils.getMessage(guild, "fatalErrorEmbedFooter"), guild.getJDA().getSelfUser().getAvatarUrl());
-                fatal.setColor(0xff5555);
-                if (Utils.getLogChannel(guild) == null) {
-                    guild.getDefaultChannel().sendMessage(fatal.build()).queue();
-                }
-                else {
-                    guild.getTextChannelById(Utils.getLogChannel(guild)).sendMessage(fatal.build()).queue();
-                }
-                fatal.clear();
-                System.out.println(info);
+        if (guild != null) {
+            switch (type) {
+                case "warning":
+                    System.out.println(info);
+                case "fatal":
+                    EmbedBuilder fatal = new EmbedBuilder();
+                    fatal.setTitle(Utils.getMessage(guild, "fatalErrorEmbedTitle"));
+                    fatal.setDescription(Utils.getMessage(guild, "fatalErrorEmbedDescription").replace("{error}", "```\n" + info + "\n```"));
+                    fatal.setFooter(Utils.getMessage(guild, "name") + " " + Utils.getMessage(guild, "fatalErrorEmbedFooter"), guild.getJDA().getSelfUser().getAvatarUrl());
+                    fatal.setColor(0xff5555);
+                    if (Utils.getLogChannel(guild) == null) {
+                        guild.getDefaultChannel().sendMessage(fatal.build()).queue();
+                    }
+                    else {
+                        guild.getTextChannelById(Utils.getLogChannel(guild)).sendMessage(fatal.build()).queue();
+                    }
+                    fatal.clear();
+                    System.out.println(info);
             }
+        }
+        else {
+            // Do something else if guild is null
+        }
     }
 }
