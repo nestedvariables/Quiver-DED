40e3ba1fba23bf79d5c3370c181db70d68a8b9c9
ExZiByte <exzibyte@gmail.com>
2019-01-18 06:01:16 -0600

    Started building server information embed need to finish

--- src/main/java/com/nestedvariables/dev/Discord/Quiver/events/information/ServerInfo.java
+++ src/main/java/com/nestedvariables/dev/Discord/Quiver/events/information/ServerInfo.java
@@ -11,9 +11,14 @@ public class ServerInfo extends ListenerAdapter {
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
         String[] args = event.getMessage().getContentRaw().split("\\s+");
 
-        if(args[0].equalsIgnoreCase(Utils.getPrefix(event.getGuild()) + "serverinfo")){
+        if(args[0].equalsIgnoreCase(Utils.getPrefix(event.getGuild()) + Utils.getMessage(event.getGuild(), "serverInfoCommand"))){
+            Integer memberCount = event.getGuild().getMembers().size();
+            Integer shardID = event.getJDA().getShardInfo().getShardId();
             EmbedBuilder eb = new EmbedBuilder();
             
+            eb.setTitle(Utils.getMessage(event.getGuild(), "guildInformationEmbedTitle").replace("{guild}", event.getGuild().getName()));
+            eb.setDescription(Utils.getMessage(event.getGuild(), "guildInformationEmbedDescription").replace("{guild}", event.getGuild().getName()).replace("{memberCount}",  memberCount.toString()).replace("{shardID}", shardID.toString()));
+            eb.setFooter(Utils.getMessage(event.getGuild(), "name") + " " + Utils.getMessage(event.getGuild(), "guildInformationEmbedFooter"), event.getJDA().getSelfUser().getAvatarUrl());
             
         }
     } 
