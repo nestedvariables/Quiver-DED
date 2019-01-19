451a66bb866d0f31f684efd20ea5e4b2bc10bbe5
ExZiByte <exzibyte@gmail.com>
2019-01-19 06:00:56 -0600

    Added read from Tokens.startupWebhook

--- src/main/java/com/nestedvariables/dev/Discord/Quiver/events/ReadyListener.java
+++ src/main/java/com/nestedvariables/dev/Discord/Quiver/events/ReadyListener.java
@@ -2,6 +2,7 @@ package com.nestedvariables.dev.Discord.Quiver.events;
 
 import com.nestedvariables.dev.Discord.Quiver.Logger;
 import com.nestedvariables.dev.Discord.Quiver.Main;
+import com.nestedvariables.dev.Discord.Quiver.Tokens;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -16,11 +17,10 @@ public class ReadyListener extends ListenerAdapter {
 
     public void onReady(ReadyEvent event) {
         Integer shardID = event.getJDA().getShardInfo().getShardId();
-        String webhook = "https://canary.discordapp.com/api/webhooks/531608886109667328/Ynfb9FDGMikJz3QE-RqUaj4nirTHnCZ-PrI4IoxIgQNkk1AJVUxCwT5PW6W0PfmaAjH8";
         HttpClient httpClient = HttpClientBuilder.create().build();
         try {
 
-            HttpPost post = new HttpPost(webhook);
+            HttpPost post = new HttpPost(Tokens.startupWebhook);
             StringEntity params = new StringEntity("details={\"embeds\":[{\"title\": \"Shard Spawned!\",\"description\": \"**Shard ID:** \"" + shardID + "\"\n**Shard Guild Count:** \"" + event.getGuildTotalCount() + "\"\n**Shard User Count:** \"" + event.getJDA().getUsers().size() + "\"\n**Guild Total:** \"" + Main.shardManager.getGuilds().size() + "\"\n**User Total:** \"" + Main.shardManager.getUsers().size(), "\"color\": 6212863,\"footer\": {\"text\" : \"Quiver Shard Spawned\",\"icon_url\": \"https://raw.githubusercontent.com/NestedVariables/Quiver/master/Quiver.png\"}]}");
             post.addHeader("content-type", "application/json");
             post.setEntity(params);
