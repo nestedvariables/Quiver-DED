bb6a4136de3ed187d4f5e9c678c6bd7f8fabff6f
ExZiByte <exzibyte@gmail.com>
2019-01-19 06:32:42 -0600

    Removed Logo from Info.java

--- src/main/java/com/nestedvariables/dev/Discord/Quiver/Info.java
+++ src/main/java/com/nestedvariables/dev/Discord/Quiver/Info.java
@@ -1,7 +1,6 @@
 package com.nestedvariables.dev.Discord.Quiver;
 
 public class Info {
-    public static String LOGO = "https://raw.githubusercontent.com/NestedVariables/Quiver/master/Quiver.png";
     // Color Definitions
     public static Integer ERROR_RED = 0xff5555;
     public static Integer Announce = 0x2018c2;
