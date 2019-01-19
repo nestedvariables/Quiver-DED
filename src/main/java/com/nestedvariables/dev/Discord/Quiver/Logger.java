2f4846cff7a4946c227429195785da74923ec888
ExZiByte <exzibyte@gmail.com>
2019-01-19 12:59:42 -0600

    Added output for Logger if Guild is null

--- src/main/java/com/nestedvariables/dev/Discord/Quiver/Logger.java
+++ src/main/java/com/nestedvariables/dev/Discord/Quiver/Logger.java
@@ -1,6 +1,7 @@
 package com.nestedvariables.dev.Discord.Quiver;
 
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.Guild;
 
 public class Logger {
@@ -9,26 +10,34 @@ public class Logger {
     public static void log(String type, String info, Guild guild) {
         if (guild != null) {
             switch (type) {
-                case "warning":
-                    System.out.println(info);
-                case "fatal":
-                    EmbedBuilder fatal = new EmbedBuilder();
-                    fatal.setTitle(Utils.loggerMessages.get("fatalErrorEmbedTitle"));
-                    fatal.setDescription(Utils.loggerMessages.get("fatalErrorEmbedDescription").replace("{error}", "```\n" + info + "\n```"));
-                    fatal.setFooter(Utils.loggerMessages.get("fatalErrorEmbedFooter"), guild.getJDA().getSelfUser().getAvatarUrl());
-                    fatal.setColor(0xff5555);
-                    if (Utils.getLogChannel(guild) == null) {
-                        guild.getDefaultChannel().sendMessage(fatal.build()).queue();
-                    }
-                    else {
-                        guild.getTextChannelById(Utils.getLogChannel(guild)).sendMessage(fatal.build()).queue();
-                    }
-                    fatal.clear();
-                    System.out.println(info);
+            case "warning":
+                System.out.println(info);
+            case "fatal":
+                EmbedBuilder fatal = new EmbedBuilder();
+                fatal.setTitle(Utils.loggerMessages.get("fatalErrorEmbedTitle"));
+                fatal.setDescription(Utils.loggerMessages.get("fatalErrorEmbedDescription").replace("{error}",
+                        "```\n" + info + "\n```"));
+                fatal.setFooter(Utils.loggerMessages.get("fatalErrorEmbedFooter"),
+                        guild.getJDA().getSelfUser().getAvatarUrl());
+                fatal.setColor(Utils.embedColor("error"));
+                if (Utils.getLogChannel(guild) == null) {
+                    guild.getDefaultChannel().sendMessage(fatal.build()).queue();
+                } else {
+                    guild.getTextChannelById(Utils.getLogChannel(guild)).sendMessage(fatal.build()).queue();
+                }
+                fatal.clear();
+                System.out.println(info);
             }
-        }
-        else {
-            // Do something else if guild is null
+        } else {
+            EmbedBuilder fatal = new EmbedBuilder();
+            fatal.setTitle(Utils.loggerMessages.get("fatalErrorEmbedTitle"));
+            fatal.setDescription(Utils.loggerMessages.get("fatalErrorEmbedDescription").replace("{error}", "```\n" + info + "\n```"));
+            fatal.setFooter(Utils.loggerMessages.get("fatalErrorEmbedFooter"), Main.shardManager.getApplicationInfo().getJDA().getSelfUser().getAvatarUrl());
+            fatal.setColor(Utils.embedColor("error"));
+
+            Main.shardManager.getGuildById("488137783127572491").getTextChannelById("517756124846358529").sendMessage(fatal.build()).queue();
+            fatal.clear();
+            System.out.println(info);
         }
     }
 }
