84303913b8358d7aab8edfbe955bdef22ee34420
Mykyta Komarn <nkomarn@hotmail.com>
2019-02-07 21:03:33 -0800

    Arrow API begins!

--- src/main/java/com/nestedvariables/dev/Discord/Quiver/util/Embeds.java
+++ src/main/java/com/nestedvariables/dev/Discord/Quiver/util/Embeds.java
@@ -16,7 +16,6 @@ public class Embeds {
     
     // Bot Welcome Embed
     public EmbedBuilder joinWelcome(GuildJoinEvent event) {
-        Data data = new Data();
         embed.setColor(color());
         embed.setDescription("Phew, I made it over the ~waves~ intact. Oh, hey there, I'm " + event.getJDA().getSelfUser().getAsMention() + " and I'd like to thank you for adding me to your Discord server, it feels pretty toasty in here. Here's some stuff you should know about me:");
         embed.addField("I'm known as", event.getJDA().getSelfUser().getAsMention(), true);
